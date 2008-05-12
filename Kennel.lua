18e33936e0c88b02e661f33d1af65746d1a66fc5
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-12 01:16:41 -0600

    De-dongle

--- Kennel.lua
+++ Kennel.lua
@@ -12,69 +12,7 @@ local pets = {
 }
 
 
-Kennel = DongleStub("Dongle-1.0"):New("Kennel")
-
-
-function Kennel:Enable()
-	self:RegisterEvent("BANKFRAME_OPENED")
-	self:RegisterEvent("BANKFRAME_CLOSED")
-end
-
-
-------------------------------
---			Event handlers			--
-------------------------------
-
-function Kennel:BANKFRAME_CLOSED()
-	if bankopen and swapped and FuBar_CorkFu then FuBar_CorkFu:GetModule("Minipet"):ActivatePet() end
-	bankopen = nil
-end
-
-
-function Kennel:BANKFRAME_OPENED()
-	bankopen = true
-	swapped = false
-
-	pets.normal.player.num = 0
-	pets.normal.bank.num = 0
-	pets.holiday.player.num = 0
-	pets.holiday.bank.num = 0
-
-	for bag=-1,11 do
-		local bagset = (bag <= 4) and (bag >= 0) and "player" or "bank"
-		for slot=1,GetContainerNumSlots(bag) do
-			local itemLink = GetContainerItemLink(bag, slot)
-			if (itemLink and pt(itemLink, "Minipet")) then
-				local n = pets.normal[bagset].num + 1
-				pets.normal[bagset].num = n
-				pets.normal[bagset].bag[n] = bag
-				pets.normal[bagset].slot[n] = slot
-			elseif (itemLink and pt(itemLink, "Minipet - Holiday")) then
-				local n = pets.holiday[bagset].num + 1
-				pets.holiday[bagset].num = n
-				pets.holiday[bagset].bag[n] = bag
-				pets.holiday[bagset].slot[n] = slot
-			end
-		end
-	end
-
-	if pets.normal.player.num > pets.normal.bank.num then
-		pets.normal.player, pets.normal.bank = pets.normal.bank, pets.normal.player
-	end
-	if pets.holiday.player.num > pets.holiday.bank.num then
-		pets.holiday.player, pets.holiday.bank = pets.holiday.bank, pets.holiday.player
-	end
-	self:DoSwaps("normal")
-	self:DoSwaps("holiday")
-
-	if (pets.normal.player.num > 0 and pets.normal.bank.num > 0)
-		or (pets.holiday.player.num > 0 and pets.holiday.bank.num > 0) then
-		swapped = true
-	end
-end
-
-
-function Kennel:DoSwaps(set)
+local function DoSwaps(set)
 	local l, g = pets[set].player, pets[set].bank
 	if l.num == 0 then return end
 	for i=1,l.num do
@@ -87,3 +25,41 @@ function Kennel:DoSwaps(set)
 	end
 end
 
+
+------------------------------
+--			Event handlers			--
+------------------------------
+
+local f = CreateFrame("frame")
+f:RegisterEvent("BANKFRAME_OPENED")
+f:RegisterEvent("BANKFRAME_CLOSED")
+f:SetScript("OnEvent", function(self, event, ...)
+	if event == "BANKFRAME_CLOSED" then
+		if bankopen and swapped and FuBar_CorkFu then FuBar_CorkFu:GetModule("Minipet"):ActivatePet() end
+		bankopen = nil
+	else
+		bankopen, swapped = true, false
+		pets.normal.player.num, pets.normal.bank.num, pets.holiday.player.num, pets.holiday.bank.num = 0, 0, 0, 0
+
+		for bag=-1,11 do
+			local bagset = (bag <= 4) and (bag >= 0) and "player" or "bank"
+			for slot=1,GetContainerNumSlots(bag) do
+				local itemLink = GetContainerItemLink(bag, slot)
+				if (itemLink and pt(itemLink, "Minipet")) then
+					local n = pets.normal[bagset].num + 1
+					pets.normal[bagset].num, pets.normal[bagset].bag[n], pets.normal[bagset].slot[n] = n, bag, slot
+				elseif (itemLink and pt(itemLink, "Minipet - Holiday")) then
+					local n = pets.holiday[bagset].num + 1
+					pets.holiday[bagset].num, pets.holiday[bagset].bag[n], pets.holiday[bagset].slot[n] = n, bag, slot
+				end
+			end
+		end
+
+		if pets.normal.player.num > pets.normal.bank.num then pets.normal.player, pets.normal.bank = pets.normal.bank, pets.normal.player end
+		if pets.holiday.player.num > pets.holiday.bank.num then pets.holiday.player, pets.holiday.bank = pets.holiday.bank, pets.holiday.player end
+		DoSwaps("normal")
+		DoSwaps("holiday")
+
+		if (pets.normal.player.num > 0 and pets.normal.bank.num > 0) or (pets.holiday.player.num > 0 and pets.holiday.bank.num > 0) then swapped = true end
+	end
+end
