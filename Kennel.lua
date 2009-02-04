0363cc35a5f0a62fdd722653af38bf6240c1b3bf
Tekkub <tekkub@gmail.com>
2009-02-04 01:34:22 -0700

    Don't put pets out when priests proc improved death

--- Kennel.lua
+++ Kennel.lua
@@ -44,7 +44,7 @@ f:SetScript("OnUpdate", function(self, elap)
 	local _, instanceType = IsInInstance()
 	local pvpink = instanceType == "pvp" or instanceType == "arena"
 
-	if pvpink or InCombatLockdown() or IsStealthed() or IsMounted() or IsFlying() or UnitCastingInfo("player") or blistzones[GetSubZoneText()] then
+	if pvpink or InCombatLockdown() or IsStealthed() or IsMounted() or IsFlying() or UnitCastingInfo("player") or blistzones[GetSubZoneText()] or UnitBuff("player", "Spirit of Redemption") then
 		elapsed = 0
 		return
 	end
