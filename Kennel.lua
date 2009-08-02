2a6ff0651e38b77183672ac6387b0f88d8e282ff
tekkub <tekkub@gmail.com>
2009-08-02 17:54:28 -0600

    No petting while falling

--- Kennel.lua
+++ Kennel.lua
@@ -80,7 +80,7 @@ f:SetScript("OnUpdate", function(self, elap)
 	local _, instanceType = IsInInstance()
 	local pvpink = instanceType == "pvp" or instanceType == "arena"
 
-	if pvpink or InCombatLockdown() or IsStealthed() or IsMounted() or IsFlying() or UnitCastingInfo("player") or UnitChannelInfo("player") or blistzones[GetSubZoneText()]
+	if pvpink or InCombatLockdown() or IsStealthed() or IsMounted() or IsFlying() or IsFalling() or UnitCastingInfo("player") or UnitChannelInfo("player") or blistzones[GetSubZoneText()]
 		or UnitBuff("player", SOR) or UnitBuff("player", FOOD) or UnitBuff("player", DRINK) then
 		elapsed = 0
 		return
