14ed5cb89ee6e7986ce1d4bb611af3181a51decb
tekkub :bear:  <tekkub@gmail.com>
2012-12-01 02:39:47 -0700

    Move config to the main file

--- Kennel.lua
+++ Kennel.lua
@@ -56,13 +56,26 @@ local function PutTheCatOut()
 end
 
 
+local function MakeButt()
+	local check = ns.NewCheckBox(PetJournal, 22, "BOTTOMLEFT", 168, 2)
+	check:SetScript("OnClick", function(self) ns.disabled = not ns.disabled end)
+	check:SetChecked(true)
+
+	ns.NewCheckLabel(check, 'Auto-summon')
+
+	ns.UnregisterEvent("ADDON_LOADED")
+	ns.ADDON_LOADED = nil
+	MakeButt = nil
+end
+
+
 ns.RegisterEvent("ADDON_LOADED", function(event, addon)
-	if addon == 'Blizzard_PetJournal' then ns.makebutt()
+	if addon == 'Blizzard_PetJournal' then MakeButt()
 	elseif addon == myname then
 		if IsLoggedIn() then PutTheCatOut()
 		else ns.RegisterEvent("PLAYER_LOGIN", PutTheCatOut) end
 
-		if IsAddOnLoaded("Blizzard_PetJournal") then ns.makebutt() end
+		if IsAddOnLoaded("Blizzard_PetJournal") then MakeButt() end
 	end
 end)
 
